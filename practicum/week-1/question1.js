f4e4a2efe50cfc7cff6919185e99abf6d883d2e7
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-08-27 13:35:50 -0700

    Update question1.js

--- practicum/week-1/question1.js
+++ practicum/week-1/question1.js
@@ -8,10 +8,11 @@ For example given the input array [5, 6, 1], the running sum is
 var runningSum = function(input) {
     var prime = 0
     var retval = []
-    for (var x = 0; x <4; x++){
+    for (var x = 0; x < input.length; x++){
         let newNum = prime + input[x];
         retval.push(newNum);
         prime = newNum;
+        console.alert(retval);
     }
     return retval;
 };
