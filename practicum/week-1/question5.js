95a5910b66a304bd18d320557cc61dbdde4a23d5
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-08-27 20:10:07 -0700

    Update question5.js

--- practicum/week-1/question5.js
+++ practicum/week-1/question5.js
@@ -12,6 +12,17 @@ output: 3
 */
 
 var minimalReduction = function(n) {
+    var opp = 0;
+    while (!(n=0)) {
+        opp++;
+        if (n % 4 === 0){
+            n %= 4;
+        }else if (n % 3 === 0){
+            n %= 3;
+        }else{
+            n -= 1;
+        }
+    } return opp;
 
 };
 
