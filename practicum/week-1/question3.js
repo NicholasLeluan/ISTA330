2dfec4ac70d180f2838d63590fe27d02391339c6
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-08-27 17:41:11 -0700

    Update question3.js

--- practicum/week-1/question3.js
+++ practicum/week-1/question3.js
@@ -9,5 +9,22 @@ There are 5 ordered pairs: (4, 6), (4, 9), (2, 6), (2, 9)
 */
 
 var numberOfOrderedPairs = function(input) {
+    var retval = [];
+        for (var x = 0; x < input.length; x++){
+            if (input.length - x === 0) {
+                return retval.length;
+            }
+            else {
+                for (x+1; x < input.length - x; x++){
+                    if (input[x]<input[x+1]){
+                        let pair = input[x].toString().concat(input[x+1]);
+                        if (retval.indexOf(pair) === -1){
+                            retval.push(pair);
+                        }
+                    }
+
+                } return retval.length;
+            }
+    }
 
 };
