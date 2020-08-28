62ee8224767f0fc4bf779cc58abe3e5925d04edd
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-08-27 19:51:31 -0700

    Update question4.js

--- practicum/week-1/question4.js
+++ practicum/week-1/question4.js
@@ -11,5 +11,20 @@ The letters in A are guaranteed distinct, and all characters in A and B are lett
 */
 
 var howManyCommon = function(A, B) {
+    var count = 0;
+    if (A.length < B.length) {
+        var short = A;
+        var long = B;
+    }else{
+        var short = B;
+        var long = A;
+    }
+    for (var k = 0; k <= long.short; k++){
+        var curr = short.charAt(k);
+        if (long.contains(short)){
+            count++;
+        }
+
+    } return count;
 
 };
