ad9b09b619996c5d957a74b205e304a73be8d9b7
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-08-30 20:19:13 -0700

    Update question8.js

--- practicum/week-1/question8.js
+++ practicum/week-1/question8.js
@@ -8,5 +8,14 @@ output: [1, 0, 1, 2]
 */
 
 var biggerAndEven = function(input) {
+    var retArray = [];
+    for (var curr = 0; curr < input.length;cur++){
+        var pushVal = 0;
+        for (var temp = 0; temp < input.length; temp++){
+            if (input[temp] > input[curr] && input[temp]%2===0){
+                pushVal++;
+            }
+        }
+    } return retArray;
 
 };
