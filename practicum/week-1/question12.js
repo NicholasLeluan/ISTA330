40661d917bec65238675e85be94f3c28c5f0a04e
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-09-01 12:45:04 -0700

    Update question12.js

--- practicum/week-1/question12.js
+++ practicum/week-1/question12.js
@@ -19,10 +19,10 @@ var maxBalanceNumber = function(input) {
   var a = 0;
   var b = 0;
   for (var curr = 0; curr < input.length; cur++){
-    if (input.charAt(curr).equals('a')){
+    if (input.charAt(curr) === ('a')){
       a++;
       subString.concat('a');
-    }else if (input.charAt(curr).equals('b')){
+    }else if (input.charAt(curr) === ('b')){
       b++;
       subString.concat('b');
     }if (a = b){
