3b021961018bb30c63071893f44f4e1c2c0861fd
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-09-03 11:31:53 -0700

    Update question7.js

--- practicum/week-1/question7.js
+++ practicum/week-1/question7.js
@@ -12,8 +12,15 @@ output: 4
 
 var maxSumOfMins = function(input) {
        /* pair the even indexes with even; odd with odd
+       CANNOT USE SORT!!
        */
-      var input = input.sort();
+      for (var k = 0; k < input.length; k++){
+             for (var i = 0; i < input.length; i++){
+                    var temp = input[i];
+                    input[i] = input[i+1];
+                    input[i+1] = temp;
+             }
+      }
       var retVal = 0;
       for (var x = 0; x < input.length; x += 2){
        retVal+= Math.min(input[x],input[x+1]);
