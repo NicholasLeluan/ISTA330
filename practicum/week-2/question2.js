5d73448fbd3d7884f6f0b0a924d8c332b8afbdd6
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-09-07 18:30:00 -0700

    Update question2.js

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -20,8 +20,6 @@ Example: input: [-12,3,-1,5,-2,1,-7]
                      for (var x = 0; x < arraySlice.length; x ++){
                              sliceSum += arraySlice[x]
                      }
-                     console.log(arraySlice);
-                     console.log(sliceSum);
                      if (sliceSum > max){
                              max = sliceSum;
                              var start = k
