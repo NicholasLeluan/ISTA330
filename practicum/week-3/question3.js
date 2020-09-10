4650b8e0e4a17bedb1b32ccc67706e2df91a4e21
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-09-09 23:33:39 -0700

    Update question3.js

--- practicum/week-3/question3.js
+++ practicum/week-3/question3.js
@@ -9,5 +9,17 @@ output: true
 */
 
 var kStepAway = function(input, k) {
-
+    for (var step = 0; step < input.length; step++){
+        if (input[step] === 1 && step ===0){
+            var zeros = 0;
+            continue;
+        }if (input[step] === 1){
+            if (!zeros === k){
+                return false;
+            }
+        }if (input[step] === 0){
+            zeros++;
+        }
+    }
+    return true;
 };
