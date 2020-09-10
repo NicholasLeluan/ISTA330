428f0afda1b4f3a023a2f2a0e6f84af7a51f7179
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-09-09 23:24:38 -0700

    Update question2.js

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -22,7 +22,8 @@ Example: input: [19,3,2,10,8,2,3,5]
          }
      }
      for (var key in dict){
-         if(key===2){
+         console.log(dict[key])
+         if(dict[key].equals(2)){
             retArray.push(key);
          }
      }
