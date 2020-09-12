189b7317f69476ddcca6407c5393ea85fb5f7baa
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-09-11 18:26:30 -0700

    Update question1.js

--- practicum/week-3/question1.js
+++ practicum/week-3/question1.js
@@ -19,8 +19,8 @@ Return the number of teams you can form given the conditions. (soldiers can be p
 */
 
 var howManyTeams = function(input) {
-        for (var i = 0;i < input.length()){
-                for (var j = 1; j < input.length - i; i++){
+        for (var i = 0;i < input.length();i++){
+                for (var j = 1; j < input.length - i; j++){
                         for (var k = 2; k < input.length -i -j;k++){
                                 var platoon = [input[i],input[j],input[k]]
                                 console.log(platoon);
