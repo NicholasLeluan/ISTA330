e37046554e3295d7742e1f4301bebb7ada45d8df
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-09-15 18:57:10 -0700

    Update question9.js

--- practicum/week-3/question9.js
+++ practicum/week-3/question9.js
@@ -11,5 +11,20 @@ output: 1
 */
 
 var minMoves = function(A) {
+    var visited = [];
+    var totalIncrements = 0;
+    for (var x = 0; x < A.length; x++){
+        if (!(visited.includes(A[x]))){
+            visited.push(A[x]);
+        }else{
+            var check = A[x];
+            while (visited.includes(check)){
+                check += 1;
+                totalIncrements++;
+            }
+            visited.push(check);
+        }
+    }
+    console.log(visited);
 
 };
