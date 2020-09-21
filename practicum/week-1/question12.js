bf64bd9cc6f833fb98044c7016b57f74b44e3b87
NicholasLeluan <70127512+NicholasLeluan@users.noreply.github.com>
2020-09-21 12:36:00 -0700

    Update question12.js

--- practicum/week-1/question12.js
+++ practicum/week-1/question12.js
@@ -3,34 +3,26 @@ A string, input, which contains only letters a and b is given.
 A list of substrings s1, s2, .., sn is called a partition for input if and only if
   input == s1 + s2 + ... + sn.
 
+<<<<<<< Updated upstream
   
 A substring is balanced if it has equal number of a's and b's.
 The number of balanced substrings in a partition is called the balence number of the partition.
+=======
+A substring is balanced if it has equal number of a's and b's.
+The number of balanced substrings in a partition is called the balanced number of the partition.
+>>>>>>> Stashed changes
 Among all the possible partitions of the input string, what is the maximum balance number?
 
 Example:
 input: 'abaabbabab'
 output: 4 because the following partition has the highest number of balanced substrings:
           'ab', 'aabb', 'ab', 'ab'
+
+          MY NOTE: 
 */
 
 var maxBalanceNumber = function(input) {
-  var retVal = 0;
-  var a = 0;
-  var b = 0;
-  for (var curr = 0; curr < input.length; curr++){
-    if (input.charAt(curr) === 'a'){
-      a++;
-      console.log(a,retVal);
-    }if (input.charAt(curr) === 'b'){
-      b++;
-      console.log(b,retVal);
-    }if (a == b){
-      retVal++;
-      var a = 0;
-      var b = 0;
-    }
-  }
-  return retVal;
+    var mid = Math.floor(input.length/2);
+
 
 };
